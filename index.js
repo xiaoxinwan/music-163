759adf46623c40577b57cbc2834f0c5e2faad328
xiaoxinwan <lixinxi1010@gmail.com>
2018-10-22 13:31:59 +0800

    热歌榜动态获取

--- index.js
+++ index.js
@@ -19,60 +19,84 @@ $(function() {
             </li>
             `)
             $('#latestMusic').append($li)
+                // $('#hotmusicPlaylist').append($li)
         })
         $('#latestMusicLoading').remove()
     });
 
-    $('.siteNav').on('click','ol.tabItems>li',function(e){
+    $.get('./songs.json').then(function(response) {
+        let items = response
+        items.forEach((i) => {
+            
+            
+            let $li = $(`
+            <li>
+            <a class="song" href="./song.html?id=${i.id}">
+                <span class="serialNum"></span>
+                <div class="song-wrapper">
+                    <div class="songInfo">
+                        <span class="name">${i.name}</span>
+                        <span class="artist">${i.artist}</span>
+                    </div>
+                    <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-play1"></use>
+                    </svg>
+                </div>
+            </a>
+            </li>
+            `)
+            $('#hotmusicPlaylist').append($li)
+        })
+    })
+
+    $('.siteNav').on('click', 'ol.tabItems>li', function(e) {
         let $li = $(e.currentTarget).addClass('active')
         $li.siblings().removeClass('active')
-        let index = $li.index()  //返回索引值，知道是哪一个
+        let index = $li.index() //返回索引值，知道是哪一个
         $('.tabContent > li').eq(index).addClass('active')
             .siblings().removeClass('active')
     })
 
     let timer = undefined
-    $('input#searchSong').on('input',function(e){
+    $('input#searchSong').on('input', function(e) {
         $('label.searchInner').text('')
         $('#output').addClass('active')
         let $input = $(e.currentTarget)
         let value = $input.val().trim()
-        if(value === ''){return}
-        if(timer){
+        if (value === '') { return }
+        if (timer) {
             clearTimeout(timer)
         }
-        timer = setTimeout(function(){
-            search(value).then((result)=>{
+        timer = setTimeout(function() {
+            search(value).then((result) => {
                 timer = undefined
-                if(result.length !== 0){
-                    $('#output').text(result.map((r)=>r.name).join(','))
-                }else{
+                if (result.length !== 0) {
+                    $('#output').text(result.map((r) => r.name).join(','))
+                } else {
                     $('#output').text('无结果')
                 }
             })
-        },400)
-   })
+        }, 400)
+    })
 
-   function search(keyword){
-       console.log('搜索'+ keyword);
-       return new Promise((resolve, reject)=>{
-           var database = [
-               {"id":"1", "name":"岁月神偷"},
-               {"id":"2", "name":"追梦赤子心"},
-               {"id":"3", "name":"美丽之最"},
-               {"id":"4", "name":"花樽与花"}
-           ]
-           let result = database.filter(function(item){
-               return item.name.indexOf(keyword)>=0
-           })
-           setTimeout(function(){
-               console.log('搜到'+keyword +'的结果')
-               resolve(result)
-           },(Math.random()*500 + 500))
-       })
-   }
+    function search(keyword) {
+        console.log('搜索' + keyword);
+        return new Promise((resolve, reject) => {
+            var database = [
+                { "id": "1", "name": "岁月神偷" },
+                { "id": "2", "name": "追梦赤子心" },
+                { "id": "3", "name": "美丽之最" },
+                { "id": "4", "name": "花樽与花" }
+            ]
+            let result = database.filter(function(item) {
+                return item.name.indexOf(keyword) >= 0
+            })
+            setTimeout(function() {
+                console.log('搜到' + keyword + '的结果')
+                resolve(result)
+            }, (Math.random() * 500 + 500))
+        })
+    }
 
-   window.search = search
+    window.search = search
 })
-
-
