ffc14e1712a51b101b6d5a342c453f7339363131
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-12 08:45:11 +0800

    推荐歌曲页 搜索页完成

--- index.js
+++ index.js
@@ -1,48 +1,66 @@
 $(function() {
-    $.get('./songs.json').then(function(response) {
+    $.get('//localhost:3000/personalized?limit=6').then(function(response) {
         //response 可能是字符串
-        let items = response
+
+        let items = response.result
         items.forEach((i) => {
             let $li = $(`
-            <li>
-                <a href="./song.html?id=${i.id}">
-                    <div class="song">
-                        <div class="songtitle">${i.name}</div>
-                        <div class="artist">${i.artist}-专辑1</div>
-                    </div>
-                    <span>
+                <li>
+                    <a href="./playlist.html?id=${i.id}">
+                        <img src=${i.picUrl} alt="">
+                        <p>${i.name}</p>
+                    </a>
+                </li>
+            `)
+            $('#recomSongList').append($li)
+        })
+        // items.forEach((i) => {
+        //
+        //
+        // })
+        $('#latestMusicLoading').remove()
+    });
+    $.get('//localhost:3000/personalized/newsong').then(function (response) {
+        let items = response.result
+        items.forEach((i) => {
+            let $li = $(`
+             <li>
+                 <a href="./song.html?id=${i.id}">
+                     <div class="song">
+                         <div class="songtitle">${i.song.name}</div>
+                         <div class="artist">${i.song.artists[0].name}-${i.song.album.name}</div>
+                     </div>
+                     <span>
                         <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#icon-play1"></use>
-                    </svg>
-                    </span>
-                </a>
-            </li>
+                             <use xlink:href="#icon-play1"></use>
+                     </svg>
+                     </span>
+                 </a>
+             </li>
             `)
             $('#latestMusic').append($li)
         })
-        $('#latestMusicLoading').remove()
-    });
+    })
+    $.get('//localhost:3000/top/list?idx=1').then(function(response) {
+        let items = response.playlist.tracks
 
-    $.get('./songs.json').then(function(response) {
-        let items = response
-        items.forEach((i) => {
-            
-            
+        let arr = items.splice(0,10)
+        arr.forEach((i,index) => {
             let $li = $(`
-            <li>
-            <a class="song" href="./song.html?id=${i.id}">
-                <span class="serialNum"></span>
-                <div class="song-wrapper">
-                    <div class="songInfo">
-                        <span class="name">${i.name}</span>
-                        <span class="artist">${i.artist}</span>
-                    </div>
-                    <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#icon-play1"></use>
-                    </svg>
-                </div>
-            </a>
-            </li>
+                <li>
+                    <a class="song" href="./song.html?id=${i.id}">
+                        <span class="serialNum">${index+1}</span>
+                        <div class="song-wrapper">
+                            <div class="songInfo">
+                                <span class="name">${i.name}</span>
+                                <span class="artist">${i.ar[0].name}</span>
+                            </div>
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-play1"></use>
+                            </svg>
+                        </div>
+                    </a>
+                </li>
             `)
             $('#hotmusicPlaylist').append($li)
         })
@@ -56,46 +74,92 @@ $(function() {
             .siblings().removeClass('active')
     })
 
-    let timer = undefined
-    $('input#searchSong').on('input', function(e) {
-        $('label.searchInner').text('')
-        $('#output').addClass('active')
-        let $input = $(e.currentTarget)
-        let value = $input.val().trim()
-        if (value === '') { return }
-        if (timer) {
-            clearTimeout(timer)
-        }
-        timer = setTimeout(function() {
-            search(value).then((result) => {
-                timer = undefined
-                if (result.length !== 0) {
-                    $('#output').text(result.map((r) => r.name).join(','))
-                } else {
-                    $('#output').text('无结果')
-                }
-            })
-        }, 400)
+    $.get('//localhost:3000/search/hot').then(function (res) {
+        let hots = res.result.hots
+        hots.forEach((i) => {
+            let $li = $(`
+                <li>${i.first}</li>
+            `)
+            $('#hotSearchList').append($li)
+        })
     })
 
-    function search(keyword) {
-        console.log('搜索' + keyword);
-        return new Promise((resolve, reject) => {
-            var database = [
-                { "id": "1", "name": "岁月神偷" },
-                { "id": "2", "name": "追梦赤子心" },
-                { "id": "3", "name": "美丽之最" },
-                { "id": "4", "name": "花樽与花" }
-            ]
-            let result = database.filter(function(item) {
-                return item.name.indexOf(keyword) >= 0
+    let func = debounce(function(e) {
+        $('label.searchInner').text('')
+        let $input = $(e.currentTarget)
+        let value = $input.val().trim()
+        if (value === '') {
+            $('.hotSearch').css('display','block')
+            $('.searchSuggest').css('display','none')
+            $('.searchSuggest').empty()
+        }
+        if (value) {
+            $('.hotSearch').css('display','none')
+            $.get(`//localhost:3000/search/suggest?keywords= ${value}`).then((res) => {
+                let result = res.result.songs
+                console.log(result)
+                if (result.length !== 0) {
+                    $('.searchSuggest').css('display','block')
+                    let $h3 = $(`
+                        <h3>搜索 "${value}"</h3>
+                        <ul id="searchSuggestList"></ul>
+                    `)
+                    $('.searchSuggest').append($h3)
+                    result.forEach((i) => {
+                        let $li = $(`
+                            <li>
+                                <svg class="searchIcon" aria-hidden="true">
+                                    <use xlink:href="#icon-search"></use>
+                                </svg>
+                                <span>
+                                    <a href="./song.html?id=${i.id}">${i.name}</a>
+                                </span>
+                            </li>
+                        `)
+                        $('#searchSuggestList').append($li)
+                    })
+                } else {
+                    $('.searchSuggest').text('无结果')
+                }
             })
-            setTimeout(function() {
-                console.log('搜到' + keyword + '的结果')
-                resolve(result)
-            }, (Math.random() * 500 + 500))
-        })
-    }
+        }
 
-    window.search = search
+        // }, 400)
+    }, 500)
+    $('input#searchSong').on('input', func)
+
+    function debounce (fn, delayTime) {
+        let timer
+        return function () {
+            clearTimeout(timer)
+            let context = this
+            let args = arguments
+            timer = setTimeout(()=>{
+                fn.apply(context,args)
+            },delayTime)
+        }
+    }
+    window.debounce = debounce
+    // })
+    //
+    // function search(keyword) {
+    //     console.log('搜索' + keyword);
+    //     return new Promise((resolve, reject) => {
+    //         var database = [
+    //             { "id": "1", "name": "岁月神偷" },
+    //             { "id": "2", "name": "追梦赤子心" },
+    //             { "id": "3", "name": "美丽之最" },
+    //             { "id": "4", "name": "花樽与花" }
+    //         ]
+    //         let result = database.filter(function(item) {
+    //             return item.name.indexOf(keyword) >= 0
+    //         })
+    //         setTimeout(function() {
+    //             console.log('搜到' + keyword + '的结果')
+    //             resolve(result)
+    //         }, (Math.random() * 500 + 500))
+    //     })
+    // }
+    //
+    // window.search = search
 })
--- song.js
+++ song.js
@@ -1,18 +1,27 @@
 $(function() {
     let id = parseInt(location.search.match(/\bid=([^&]*)/)[1],10)
-    
-    $.get('./songs.json').then(function(response) {
-        let songs = response
-        let song = songs.filter(songs =>songs.id===id)[0]
-        let { url,cover,name,artist,lyric } = song
-        
-        initPlayer.call(undefined, url)
-        initText(cover,name,artist,lyric)
+
+    $.get(`//localhost:3000/song/url?id=${id}`).then(function(response) {
+        let songs = response.data[0]
+         initPlayer.call(undefined, songs.url)
     })
-    
+    $.get(`//localhost:3000/song/detail?ids=${id}`).then(function(response) {
+        let cover = response.songs[0].al.picUrl
+        let name = response.songs[0].name
+        let artist = response.songs[0].ar[0].name
+
+        $('.cover')[0].src = cover
+        $('.page-background').css('background-image',`url(${cover})`)
+        $('.song-description>h1')[0].innerText = name + '-' + artist
+
+    })
+    $.get(`//localhost:3000/lyric?id=${id}`).then(function(response) {
+        let lyric = response.lrc.lyric
+        parseLyric(lyric)
+    })
     function initPlayer(url){
-        
-        
+
+
         let audio = document.createElement('audio')
         audio.src = url
         audio.muted = ""
@@ -29,16 +38,15 @@ $(function() {
             $('.disc-container').addClass('playing')
         })
         setInterval(()=>{
-            
+
             let seconds = audio.currentTime
             let minutes = ~~(seconds/60)
             let left = seconds - minutes*60
-            let time = `${pad(minutes)}: ${pad(left)}`
+            let time = `${pad(minutes)}:${pad(left)}`
             let $lyric = $('.lyric-wrapper > p')
             let $whichlyric
             for(let i=0; i<$lyric.length; i++){
                 if($lyric[i+1] !== undefined && $lyric.eq(i).attr('data-time') < time && $lyric.eq(i+1).attr('data-time') > time){
-                    
                     $whichlyric = $lyric.eq(i)
                     break
                 }
@@ -56,21 +64,20 @@ $(function() {
         return number > 10 ? number + '' : '0' + number
     }
     function initText(cover,name,artist,lyric){
-        $('.cover')[0].src = cover
-        $('.song-description>h1')[0].innerText = name + '-' + artist
-        parseLyric(lyric)
+
+
     }
     function parseLyric (lyric){
-        let array = lyric.split('\n')    
+        let array = lyric.split('\n')
         let regex = /^\[(.+)\](.*)$/
-        
+
         array = array.map((string) => {
                 let matches = string.match(regex)
                 if (matches) {
                     return { time: matches[1], words: matches[2] }
                 }
             })
-            // 将内容append到('.lyric')上
+        // 将内容append到('.lyric')上
         let $lyric = $('.lyric')
         array.map(function(object) {
             if (!object) { return }
@@ -80,5 +87,5 @@ $(function() {
         })
     }
 
-   
+
 })
